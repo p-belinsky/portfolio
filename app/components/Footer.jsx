1229a96ad87341e6001be8f9034e3db6dc517645
Phil Belinsky <philbelinsky@yahoo.com>
2026-01-20 14:03:47 -0500

    updated copyright year in footer to be dynamic

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = ({isDarkMode}) => {
         </div>
 
         <div className="text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6">
-            <p>© 2025 Philip Belinsky. All rights reserved.</p>
+            <p>© {new Date().getFullYear()} Philip Belinsky. All rights reserved.</p>
             <ul className="flex items-center gap-10 justify-center mt-4 sm:mt-0">
                 <li><a target="_blank" href="https://github.com/p-belinsky">GitHub</a></li>
                 <li><a target="_blank" href="https://www.linkedin.com/in/philip-belinsky-a462b8192/">LinkedIn</a></li>
